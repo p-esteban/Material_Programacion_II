5f3172504fd8bb93aaba4325ec58e5a5f551de8a
profesor <profesor>
2019-03-28 18:57:17 -0300

    Clase 4

---
+++ 04_Sobrecarga/Ejemplo_Sobrecarga_Constructores/Ejemplo_Sobrecarga_Constructores/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_Sobrecarga_Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sobrecarga sobrecarga = new Sobrecarga(3, 4, "Prueba");
+        }
+    }
+}
---
+++ 04_Sobrecarga/Ejemplo_Sobrecarga_Constructores/Ejemplo_Sobrecarga_Constructores/Sobrecarga.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_Sobrecarga_Constructores
+{
+    class Sobrecarga
+    {
+        private string mensaje;
+        private int n;
+        private int x;
+
+        public Sobrecarga()
+        {
+            this.mensaje = "Sobrecarga";
+            this.n = 5;
+            this.x = 6;
+        }
+
+        public Sobrecarga(int n) : this()
+        {
+            this.n = n;
+        }
+
+        public Sobrecarga(int n, int x) : this(n)
+        {
+            this.x = x;
+        }
+
+        public Sobrecarga(int n, int x, string mensaje) : this(n, x)
+        {
+            this.mensaje = mensaje;
+        }
+    }
+}
