682b24c1789c5e1ee0eb9b7d7b3ddfe847af2f00
Cerizza <mauricio-ariel.cerizza@capgemini.com>
2019-04-12 10:17:26 -0300

    nuevas clases y guías

---
+++ 06_WindowsForms/HolaMundoWF/HolaMundoWF/HolaMundoForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace HolaMundoWF
+{
+    partial class HolaMundoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_saludar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_saludar
+            // 
+            this.btn_saludar.Location = new System.Drawing.Point(205, 23);
+            this.btn_saludar.Name = "btn_saludar";
+            this.btn_saludar.Size = new System.Drawing.Size(75, 23);
+            this.btn_saludar.TabIndex = 0;
+            this.btn_saludar.Text = "Saludar";
+            this.btn_saludar.UseVisualStyleBackColor = true;
+            this.btn_saludar.Click += new System.EventHandler(this.BtnSaludarOnClick);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 9);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(54, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 25);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(173, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // HolaMundoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(324, 89);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btn_saludar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HolaMundoForm";
+            this.Text = "Hola Mundo!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_saludar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
---
+++ 06_WindowsForms/HolaMundoWF/HolaMundoWF/HolaMundoForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundoWF
+{
+    public partial class HolaMundoForm : Form
+    {
+        public HolaMundoForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Manejador del evento click del btnSaludar (botón). Se llamará cuando el usuario haga click. 
+        /// </summary>
+        /// <param name="sender">Referencia al control que lanza el evento.</param>
+        /// <param name="e">Contiene los argumentos del evento.</param>
+        private void BtnSaludarOnClick(object sender, EventArgs e)
+        {
+            //Obtengo el texto ingresado por el usuario en el textbox "txtNombre".
+            string nombre = this.txtNombre.Text;
+            //Muestro un MessageBox con un mensaje de bienvenida personalizado. 
+            DialogResult result = MessageBox.Show(String.Format("¡Hola {0}! Bienvenido.", nombre), "Bienvenido", MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Information);
+
+            //Cuando el usuario presione aceptar se va a cerrar el form. 
+            if (result == DialogResult.OK)
+            {
+                this.Close();
+            }
+            else //Si apretó cancelar abro una nueva ventana del HolaMundoForm. 
+            {
+                HolaMundoForm nuevoForm = new HolaMundoForm();
+                //nuevoForm.ShowDialog(); //Muestra el formulario de forma modal, es decir, no se podrá interactuar con el padre. 
+                nuevoForm.Show(); //Muestra el formulario de forma no-modal, es decir, se puede interactuar con el padre. 
+            }
+        }
+    }
+}
