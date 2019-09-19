4e343cbeb79ee77c52f7acd05426e2ed396df742
profesor <profesor>
2019-09-19 19:20:01 -0300

    Ejercicio 33

---
+++ 08_Propiedades_Enumerados_Indexadores/Solución_Ejercicio_33/Biblioteca/Libro.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        /// <summary>
+        /// Constructor. 
+        /// Instancia la lista de páginas del libro.
+        /// </summary>
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+
+        /// <summary>
+        /// Indexador del libro.
+        /// </summary>
+        /// <param name="indice">Indice de la página</param>
+        /// <returns></returns>
+        public string this[int indice] {
+            get
+            {
+                /* Si el índice es mayor o igual a cero 
+                 * y menor a la cantidad de elementos de la lista. 
+                 * Entonces, existe.
+                 * Lo retorno.                
+                 */
+                if (indice >= 0 && indice < this.paginas.Count)
+                {
+                    return this.paginas[indice];
+                }
+                else 
+                {
+                    /* Sino, estoy tratando de acceder 
+                     * a un índice que no existe.
+                     * Retorno una cadena vacía.
+                     */
+                    return string.Empty;
+                }
+            }
+
+            set
+            {
+                // Si el índice es válido.
+                if (indice >= 0)
+                {
+                    /* Si el índice es mayor o igual a cero 
+                     * y menor a la cantidad de elementos de la lista. 
+                     * Entonces, existe.
+                     * Lo modifico.             
+                     */
+                    if (indice < this.paginas.Count)
+                    {
+                        this.paginas[indice] = value;
+                    }
+                    else
+                    {
+                        /* Si supera o iguala 
+                         * a la cantidad de elementos,
+                         * Lo agrego.                        
+                        */
+                        this.paginas.Add(value);
+                    }
+                }
+            }
+        } 
+
+    }
+}
