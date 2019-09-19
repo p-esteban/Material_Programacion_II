7ed2fc0a251115ec84379662b5b72e8292ce6b03
profesor <profesor>
2019-09-18 22:19:39 -0300

    Clase 09

---
+++ 08_Propiedades_Enumerados_Indexadores/Solucion_Ejercicio_28/Formulario/ContadorPalabrasForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Formulario
+{
+    partial class ContadorPalabrasForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTxtPalabras = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTxtPalabras
+            // 
+            this.richTxtPalabras.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTxtPalabras.Location = new System.Drawing.Point(12, 12);
+            this.richTxtPalabras.Name = "richTxtPalabras";
+            this.richTxtPalabras.Size = new System.Drawing.Size(705, 431);
+            this.richTxtPalabras.TabIndex = 0;
+            this.richTxtPalabras.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(550, 449);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(167, 27);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_OnClick);
+            // 
+            // ContadorPalabrasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 488);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.richTxtPalabras);
+            this.Name = "ContadorPalabrasForm";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtPalabras;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ 08_Propiedades_Enumerados_Indexadores/Solucion_Ejercicio_28/Formulario/ContadorPalabrasForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class ContadorPalabrasForm : Form
+    {
+        public ContadorPalabrasForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Manejador del evento OnClick del botón calcular.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCalcular_OnClick(object sender, EventArgs e)
+        {
+            // Declaro un atributo que almacenará mi diccionario (contador de palabras)
+            Dictionary<string, int> contadorPalabras;
+
+            // Las colecciones son objetos, hay que instanciarlos.
+            contadorPalabras = new Dictionary<string, int>();
+
+            string[] palabras = this.richTxtPalabras.Text.Split(' ');
+
+            foreach (string palabra in palabras)
+            {
+                string palabraAux = palabra.ToLower();
+
+                if (contadorPalabras.ContainsKey(palabraAux))
+                {
+                    contadorPalabras[palabraAux]++;
+                }
+                else
+                {
+                    contadorPalabras.Add(palabraAux, 1);
+                }
+            }
+            this.InformarMasRepetidas(contadorPalabras, 3);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>
+        /// Less than zero	This instance precedes value.
+        /// Zero This instance has the same position in the sort order as value.
+        /// Greater than zero This instance follows value. -or- value is null.
+        /// </returns>
+        private int Compare(KeyValuePair<string, int> x, KeyValuePair<string, int> y)
+        {
+            return y.Value - x.Value;
+        }
+
+        private void InformarMasRepetidas(Dictionary<string, int> contadorPalabras, int cantidadAMostrar)
+        {
+            //Ordenar de forma descendente(mayor a menor).
+          
+            // OPCION 1: Usando el método Sort de las listas.
+            // Paso el diccionario a lista para ordenar.
+            List<KeyValuePair<string, int>> lista = contadorPalabras.ToList();
+            lista.Sort(Compare);
+
+            // Versión Lambda del método Compare:
+            //lista.Sort((x, y) => y.Value - x.Value);
+
+            // OPCION 2: Ordenar usando LINQ y funciones lambda
+            // Directamente sobre el diccionario.
+            //contadorPalabras.OrderByDescending(keyValuePair => keyValuePair.Value);
+
+            // Mostrar.
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("PALABRAS MAS REPETIDAS:");
+
+            
+            for (int i = 0, j = 0; i < lista.Count && j < cantidadAMostrar; i++)
+            {
+                sb.AppendFormat("<<Puesto {0:0#}>> {1}, {2} repeticiones.\n", i + 1, lista[i].Key, lista[i].Value);
+
+                if(lista[i].Value != lista[i+1].Value)
+                {
+                    j++;
+                }
+            }
+
+            MessageBox.Show(sb.ToString(), "Palabras más repetidas", MessageBoxButtons.OK);        
+            
+
+        }
+
+        /*
+            Caso de prueba:
+            
+            "Prueba Test Prueba PRUEBA Hola hola test test gato gato prueba casa perro hola gato esto es laboratorio y programación 2 casa"
+            
+            Resultado
+            prueba 04
+            test 03
+            hola 03
+            gato 03
+            casa 02         
+         */
+    }
+}
